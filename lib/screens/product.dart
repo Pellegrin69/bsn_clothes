3c8e81f2ad83110a1f8af9998fc66c5e37a32c46
Gauthier <gotcaspar@orange.fr>
2024-04-23 10:53:37 +0200

    La page product.dart prends un id en paramètre et affiche les données de ce produit. (manque la marque, la taille, les nièmes photos et le user propriétaire de l'annonce).

--- lib/screens/product.dart
+++ lib/screens/product.dart
@@ -1,69 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ProductPage extends StatelessWidget {
   const ProductPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+
+    final String productId = ModalRoute.of(context)!.settings.arguments as String;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Produit'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          // Carrousel d'images
-          const SizedBox(
-            height: 300, // Ajustez la hauteur selon vos besoins
-            child: Carousel(
-              imagePaths: [
-                'lib/assets/image_1.jpg',
-                'lib/assets/image_2.jpg',
-                'lib/assets/image_3.jpg',
-                // Ajoutez d'autres chemins d'images ici si nécessaire
+      body: StreamBuilder<DocumentSnapshot>(
+        stream: FirebaseFirestore.instance.collection('articles').doc(productId).snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (!snapshot.hasData || !snapshot.data!.exists) {
+            return const Center(child: Text('Aucun produit trouvé.'));
+          }
+          final productData = snapshot.data!.data() as Map<String, dynamic>;
+          var image = productData['picture'];
+          return SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                // Carrousel d'images
+                SizedBox(
+                  height: 300, // Ajustez la hauteur selon vos besoins
+                  child: Carousel(
+                    imagePaths: [
+                      'lib/assets/$image',
+                      'lib/assets/image_2.jpg',
+                      'lib/assets/image_3.jpg',
+                      // Ajoutez d'autres chemins d'images ici si nécessaire
+                    ],
+                  ),
+                ),
+                // Titre
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    productData['title'] ?? '',
+                    style: const TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                // Prix
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Text(
+                    'Prix: ${productData['price']} €',
+                    style: const TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                ),
+                // Description
+                Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Text(
+                    productData['description'] ?? '',
+                    style: const TextStyle(
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ),
+                // Bouton Acheter
+                ElevatedButton(
+                  onPressed: () {
+                    // Action à effectuer lorsqu'on appuie sur le bouton Acheter
+                  },
+                  child: const Text('Acheter'),
+                ),
               ],
             ),
-          ),
-          // Titre
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'Nom du Produit',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          // Prix
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              'Prix: \$99.99',
-              style: TextStyle(
-                fontSize: 18.0,
-              ),
-            ),
-          ),
-          // Description
-          const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'Description du produit...',
-              style: TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
-          ),
-          // Bouton Acheter
-          ElevatedButton(
-            onPressed: () {
-              // Action à effectuer lorsqu'on appuie sur le bouton Acheter
-            },
-            child: const Text('Acheter'),
-          ),
-        ],
-      ),
+          );
+        },
+      )
     );
   }
 }
